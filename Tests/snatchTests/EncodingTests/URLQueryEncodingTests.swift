5b3f16a5d28ee313e011617ee6c29f8fa11dac9a
Isaac Carol Weisberg <isaac_weisberg@yahoo.com>
2018-02-23 21:37:33 +0700

    Added URLEncoding query swap test Altered testURLEncoding to expect that pairs in dictionary might be fed into the map closure in an arbitrary order

--- Tests/snatchTests/EncodingTests/URLQueryEncodingTests.swift
+++ Tests/snatchTests/EncodingTests/URLQueryEncodingTests.swift
@@ -11,12 +11,31 @@ class URLQueryEncodingTests: XCTestCase {
         ]
 
         let expectation = "name=Peter&lastname=Jackson"
+        let anotherExpectation = "lastname=Jackson&name=Peter"
 
         let result = encoding.encode(dict)
-        XCTAssert(expectation == result, "Should produce a valid encoding of the dictionary, but got this \(result)")
+        XCTAssert(expectation == result || anotherExpectation == result, "Should produce a valid encoding of the dictionary, but got this \(result)")
+    }
+
+    func testURLEncodingQuerySwap() {
+        let encoding = URLQueryEncoding()
+        
+        let baseUrl = URL(string: "https://apple.com/shitlang?objectivec=yes")!
+
+        let newQuery = "name=Peter&lastname=Jackson"
+
+        let expectation = "https://apple.com/shitlang?name=Peter&lastname=Jackson"
+
+        guard let newUrl = encoding.swapQuery(of: baseUrl, with: newQuery) else {
+            XCTFail("Should've successfully created new url")
+            return
+        }
+
+        XCTAssert(newUrl.absoluteString == expectation, "Should've successfully swapped query for a new one, but instead resulted in \(newUrl.absoluteString)")
     }
 
     static var allTests = [
         ("testURLEncoding", testURLEncoding),
+        ("testURLEncodingQuerySwap", testURLEncodingQuerySwap),
     ]
 }
