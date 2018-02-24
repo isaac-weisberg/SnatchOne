900e30864bd80984a1157accece1414f6cfd84a1
Isaac Carol Weisberg <isaac_weisberg@yahoo.com>
2018-02-24 20:49:30 +0700

    A dead obvious implementation of simple url only post request

--- Sources/snatch/Methods/Post.swift
+++ Sources/snatch/Methods/Post.swift
@@ -8,5 +8,16 @@ extension Snatch {
     class Post: SnatchModule {
         weak var father: Snatch?
         
+        subscript(_ url: URL) -> Promise<Result> {
+            guard let father = father else {
+                return SnatchError.spooks.promised
+            }
+
+            var request = URLRequest(url: url)
+
+            request.httpMethod = "POST"
+
+            return father.request(request)
+        }
     }
 }
