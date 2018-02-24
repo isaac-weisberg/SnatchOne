36d4d950d4a937f8ffdab5a8f8d4441c847dffbe
Isaac Carol Weisberg <isaac_weisberg@yahoo.com>
2018-02-24 21:07:15 +0700

    Added generalized methods to create RequestObjects Added SnatchError type called encoding error, which is to occur when an encoding error happens

--- Sources/snatch/Methods/Post.swift
+++ Sources/snatch/Methods/Post.swift
@@ -7,17 +7,42 @@ import Foundation
 extension Snatch {
     class Post: SnatchModule {
         weak var father: Snatch?
-        
+
+        /**
+            Sends an empty body, no headers POST request directly at URL.
+
+            - parameter url: the url
+
+            - returns: Promise that fulfills with Snatch.Result object.
+        */
         subscript(_ url: URL) -> Promise<Result> {
             guard let father = father else {
                 return SnatchError.spooks.promised
             }
 
+            let request = generateRequest(outOf: url, nil)
+
+            return father.request(request)
+        }
+
+        func generateRequest(outOf url: URL, _ headers: [String: String]? = nil) -> URLRequest {
             var request = URLRequest(url: url)
 
             request.httpMethod = "POST"
 
-            return father.request(request)
+            if let headers = headers {
+                request.allHTTPHeaderFields = headers
+            }
+
+            return request
+        }
+
+        func apply(_ parameters: [String: Encodable], to request: inout URLRequest) throws {
+            let encoder = JSONEncoder()
+
+            let data = try encoder.encode(parameters)
+
+            request.httpBody = data
         }
     }
 }
