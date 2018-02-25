fa34df3843674dfbfecfc05f268dcc17462cc9a3
Isaac Carol Weisberg <isaac_weisberg@yahoo.com>
2018-02-25 18:08:44 +0700

    Added test for apply(_:to:) of JSONBodyEncoding.

--- Tests/snatchTests/EncodingTests/JSONBodyEncodingTests.swift
+++ Tests/snatchTests/EncodingTests/JSONBodyEncodingTests.swift
@@ -5,14 +5,6 @@ class JSONBodyEncodingTests: XCTestCase {
     func testJSONBodyEncoding() throws {
         let encoder = JSONBodyEncoding()
 
-        let params = [
-            "name": "Ash",
-            "fame": "Trash"
-        ]
-
-        let expectationUno = "{\"name\":\"Ash\",\"fame\":\"Trash\"}" 
-        let expectationDuo = "{\"fame\":\"Trash\",\"name\":\"Ash\"}" 
-
         let result = try encoder.encode(params)
 
         guard let string = String(data: result, encoding: .utf8) else {
@@ -26,7 +18,39 @@ class JSONBodyEncodingTests: XCTestCase {
         )
     }
 
+    func testJSONBodyApplicationToARequest() throws {
+        let encoder = JSONBodyEncoding()
+
+        var req = URLRequest(url: whateverURL)
+
+        try encoder.apply(params, to: &req)
+
+        guard 
+            let data = req.httpBody,
+            let string = String(data: data, encoding: .utf8) 
+        else {
+            XCTFail("Should've been able to get the body and convert it to string, but got \(String(describing: req.httpBody))")
+            return
+        }
+
+        XCTAssert(
+            expectationUno == string || expectationDuo == string,
+            "Should've produced either out of 2 options."
+        )
+    }
+
+    let whateverURL = URL(string: "http://apple.com/genericpostroute")!
+
+    let params = [
+        "name": "Ash",
+        "fame": "Trash"
+    ]
+
+    let expectationUno = "{\"name\":\"Ash\",\"fame\":\"Trash\"}"
+    let expectationDuo = "{\"fame\":\"Trash\",\"name\":\"Ash\"}"
+
     static var allTests = [
         ("testJSONBodyEncoding", testJSONBodyEncoding),
+        ("testJSONBodyApplicationToARequest", testJSONBodyApplicationToARequest),
     ]
 }
