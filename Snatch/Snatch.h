80c293ee72a270421bbb8630b10b03aa6fed43aa
Isaac Weisberg <isaac_weisberg@yahoo.com>
2018-02-28 14:24:58 +0700

    Added an xcproj

---
+++ Snatch/Snatch.h
@@ -0,0 +1,18 @@
+//
+//  Snatch.h
+//  Snatch
+//
+//  Created by Isaac Weisberg on 2/28/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Snatch.
+FOUNDATION_EXPORT double SnatchVersionNumber;
+
+//! Project version string for Snatch.
+FOUNDATION_EXPORT const unsigned char SnatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Snatch/PublicHeader.h>
+
+
