9d61bdd0b106574f900422730cb4d50c19251894
Isaac Weisberg <isaac_weisberg@yahoo.com>
2018-03-23 15:02:45 +0700

    Add test that tests basically if program is still alive while the response comes for a POST request

--- Tests/snatchTests/MethodTests/PostTests.swift
+++ Tests/snatchTests/MethodTests/PostTests.swift
@@ -11,6 +11,19 @@ class PostTests: XCTestCase {
         XCTAssertNotNil(req.allHTTPHeaderFields, "The headers should be there.")
         XCTAssert(req.allHTTPHeaderFields! == customHeaders, "Headers should be the same thing.")
     }
+    
+    func testPostWithHeaders() {
+        let exp = expectation(description: "Should work")
+        
+        let snatch = Snatch()
+        snatch.post[ arbitraryURL, ["fuck": 3, "faffing": "over 9000", customHeaders] ].then { res in
+            exp.fulfill()
+        }.catch { error in
+            XCTFail("\(error)")
+        }
+        
+        waitForExpectations(timeout: 20.0)
+    }
 
     let customHeaders = [
         "User-agent" : "AMD286 / MS-DOS 5.0 / Chromium 32.15.133242t4",
@@ -21,5 +34,6 @@ class PostTests: XCTestCase {
 
     static var allTests = [
         ("testPostModuleRequestGeneration", testPostModuleRequestGeneration),
+        ("testPostWithHeaders", testPostWithHeaders),
     ]
 }
